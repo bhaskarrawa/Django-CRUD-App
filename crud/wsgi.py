c445b0763dd1ea6894c002cfb6afdab83973ea64
bhaskarRawat <bhaskarrawat2000@gmail.com>
2025-05-15 12:43:46 +0530

    Changed the deployment_settings.py file

--- crud/wsgi.py
+++ crud/wsgi.py
@@ -11,7 +11,7 @@ import os
 
 from django.core.wsgi import get_wsgi_application
 
-setting_module = 'crud.deployment_settings' if 'RENDER_EXTERNAL_HOSTNAME' in os.environ else 'crud.settings'
+setting_module = 'crud.deployement_settings' if 'RENDER_EXTERNAL_HOSTNAME' in os.environ else 'crud.settings'
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', setting_module)
 
 application = get_wsgi_application()
