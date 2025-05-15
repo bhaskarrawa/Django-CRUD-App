17e59dbb60c7da13201ff30cddd0d59ccf4acd6b
bhaskarRawat <bhaskarrawat2000@gmail.com>
2025-05-15 23:21:37 +0530

    Changed in deployment_setting.py file

--- crud/deployement_settings.py
+++ crud/deployement_settings.py
@@ -22,7 +22,7 @@ MIDDLEWARE = [
 ]
 
 CORS_ALLOWED_ORIGINS = [
-    'https://footballclub-of-sholin-soccer.onrender.com/'
+    'https://footballclub-of-sholin-soccer.onrender.com'
 ]
 
 STORAGES = {
